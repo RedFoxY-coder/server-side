dabeab2ee1ae1c59b4281c8e59f4eec22e3865d7
Foxy <vovka71rus1820@gmail.com>
2026-07-02 17:08:49 +0300

    ,

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -8,7 +8,7 @@ export const databaseProviders = [
     useFactory: async () => {
       const sequelize = new Sequelize({
         dialect: 'postgres',
-        host: 'postgresql://postgres:eyvQlUVtJYfegqWZpcIRHrOPvYiPfOFh@maglev.proxy.rlwy.net:59723/railway',
+        host: 'postgres.railway.internal',
         port: 5432,
         username: 'postgres',
         password: 'eyvQlUVtJYfegqWZpcIRHrOPvYiPfOFh',
