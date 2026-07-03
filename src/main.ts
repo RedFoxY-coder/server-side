b07f9201c1f1d47731b453ef8c8b394615a72a55
Foxy <vovka71rus1820@gmail.com>
2026-07-03 16:32:29 +0300

    update cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'https://memory-cards-seven-zeta.vercel.app',
+    ],
     methods: 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
     allowedHeaders:
       'Content-Type, Authorization,  Access-Control-Allow-Origin, Access-Control-Allow-Credentials',
